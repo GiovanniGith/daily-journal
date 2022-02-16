e662328bfec0d175322dae1e949551e0dedca241
GiovanniGith <Gbarajas2109@gmail.com>
2022-02-15 20:45:32 -0600

    new json file and html files

--- scripts/DailyJournal.js
+++ scripts/DailyJournal.js
@@ -1,9 +1,17 @@
-import { Entries } from "./Entries.js"
+import { Entries } from "./Entries.js";
+import { JournalForm} from "./JournalForm.JS";
 
 export const DailyJournal = () => {
-    return `
-        <div class="entryList">
-            ${Entries()} 
-            </div>
-        `
-}
+  return `
+    <header>Giovanni's Journal</header>
+    <main>
+      <h1 class="headerText">Daily Log</h1>
+      ${JournalForm()}
+        </br> </br>
+        <article id="entries">${Entries()}</article>
+        </br>
+        </br>
+    </main>
+    <footer>Aim to learn everyday! Keep on grinding!</footer>
+        `;
+};
